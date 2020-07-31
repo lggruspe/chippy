12a43106eab5b02dd7c189bd796f28a36fff485c
Levi Gruspe <mail.levig@gmail.com>
2020-07-31 17:55:13 +0800

    Parse CLI arguments

--- chippy/__main__.py
+++ chippy/__main__.py
@@ -1,11 +1,20 @@
 """Run chip8 interpreter."""
 
+from argparse import ArgumentParser
+import errno
+import os.path
 import sys
 
 from . import Chippy
 
 if __name__ == "__main__":
+    parser = ArgumentParser(prog="Chippy", description="Run chip-8 emulator.")
+    parser.add_argument("program", help="chip-8 ROM")
+    args = parser.parse_args()
+    if not os.path.isfile(args.program):
+        print(f"Program '{args.program}' not found.", file=sys.stderr)
+        sys.exit(errno.ENOENT)
+
     chip = Chippy()
-    if len(sys.argv) > 1:
-        chip.load(sys.argv[1])
+    chip.load(args.program)
     chip.run()
