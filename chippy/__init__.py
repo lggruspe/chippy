831920d857128d192f24336be574e1a157689afe
Levi Gruspe <mail.levig@gmail.com>
2020-08-02 22:27:12 +0800

    Reorganize bytecode classes

--- chippy/__init__.py
+++ chippy/__init__.py
@@ -8,85 +8,13 @@ import sys
 import threading
 import time
 
+from .code import classify
 from .window import Window
 
 class ChippyError(Exception):
     """Base chippy error."""
 
 class InstructionSet:
-    @staticmethod
-    def classify(instruction):
-        """Get name of instruction handler."""
-        if instruction in (0x00e0, 0x00ee):
-            return f"op_{instruction:04x}",
-
-        opcode = instruction >> 12
-
-        if 0 <= opcode <= 2:
-            return f"op_{opcode}nnn", instruction& 0x0fff
-        if 3 <= opcode <= 4:
-            x = instruction & 0x0f00
-            kk = instruction & 0x00ff
-            return f"op_{opcode}xkk", x >> 8, kk
-        if opcode == 5:
-            # if instruction & 0xf00f == 0x5000
-            if instruction & 0x000f == 0:
-                x = instruction & 0x0f00
-                y = instruction & 0x00f0
-                return "op_5xy0", x >> 8, y >> 4
-        if 6 <= opcode <= 7:
-            x = instruction & 0x0f00
-            kk = instruction & 0x00ff
-            return f"op_{opcode}xkk", x >> 8, kk
-        if opcode == 8:
-            function = instruction & 0x000f
-            x = instruction & 0x0f00
-            y = instruction & 0x00f0
-            if 0 <= function <= 7:
-                return f"op_8xy{function}", x >> 8, y >> 4
-            if function == 0xe:
-                return f"op_8xye", x >> 8, y >> 4
-        if opcode == 9:
-            if instruction & 0x000f == 0:
-                x = instruction & 0x0f00
-                y = instruction & 0x00f0
-                return "op_9xy0", x >> 8, y >> 4
-        if 0xa <= opcode <= 0xb:
-            return f"op_{opcode:1x}nnn", instruction & 0x0fff
-        if opcode == 0xc:
-            x = instruction & 0x0f00
-            kk = instruction & 0x00ff
-            return "op_cxkk", x >> 8, kk
-        if opcode == 0xd:
-            x = instruction & 0x0f00
-            y = instruction & 0x00f0
-            n = instruction & 0x000f
-            return "op_dxyn", x >> 8, y >> 4, n
-        if opcode == 0xe:
-            function = instruction & 0x00ff
-            x = instruction & 0x0f00
-            if function == 0x9e:
-                return "op_ex9e", x >> 8
-            if function == 0xa1:
-                return "op_exa1", x >> 8
-        if opcode == 0xf:
-            function = instruction & 0x00ff
-            if function in (0x07, 0x0a, 0x15, 0x18, 0x1e, 0x29, 0x33, 0x55, 0x65):
-                x = instruction & 0x0f00
-                return f"op_fx{function:02x}", x >> 8
-        return "",
-
-    @staticmethod
-    def decode(instruction):
-        """Return instruction handler."""
-        name, *args= InstructionSet.classify(instruction)
-        handler = getattr(InstructionSet, name, None)
-        if handler:
-            return handler, args
-        message = f"Unknown instruction: {instruction:04x}"
-        print(message, file=sys.stderr)
-        raise ChippyError(message)
-
     @staticmethod
     def op_0nnn(self, nnn):
         """Jump to routine at nnn."""
@@ -406,14 +334,20 @@ class Chippy:
         """
         self.program_counter += 2
         self.program_counter &= 0x0fff
-        '''
-        if self.program_counter < 0x200:
-            self.program_counter = 0x200
-        '''
+
+    def decode(self, instruction):
+        """Decode instruction and return instruction handler."""
+        name, *args = classify(instruction)
+        handler = getattr(InstructionSet, name, None)
+        if handler:
+            return handler, args
+        message = f"Unknown instruction: {instruction:04x}"
+        print(message, file=sys.stderr)
+        raise ChippyError(message)
 
     def execute(self, instruction):
         """Execute instruction."""
-        handler, args = InstructionSet.decode(instruction)
+        handler, args = self.decode(instruction)
         handler(self, *args)
 
     def cycle(self):
---
+++ chippy/code.py
@@ -0,0 +1,65 @@
+def classify(instruction):
+    """Classify instruction.
+
+    Return name of instruction handler and arguments.
+    """
+    if instruction in (0x00e0, 0x00ee):
+        return f"op_{instruction:04x}",
+
+    opcode = instruction >> 12
+
+    if 0 <= opcode <= 2:
+        return f"op_{opcode}nnn", instruction & 0x0fff
+    if 3 <= opcode <= 4:
+        x = instruction & 0x0f00
+        kk = instruction & 0x00ff
+        return f"op_{opcode}xkk", x >> 8, kk
+    if opcode == 5:
+        # if instruction & 0xf00f == 0x5000
+        if instruction & 0x000f == 0:
+            x = instruction & 0x0f00
+            y = instruction & 0x00f0
+            return "op_5xy0", x >> 8, y >> 4
+    if 6 <= opcode <= 7:
+        x = instruction & 0x0f00
+        kk = instruction & 0x00ff
+        return f"op_{opcode}xkk", x >> 8, kk
+    if opcode == 8:
+        function = instruction & 0x000f
+        x = instruction & 0x0f00
+        y = instruction & 0x00f0
+        if 0 <= function <= 7:
+            return f"op_8xy{function}", x >> 8, y >> 4
+        if function == 0xe:
+            return f"op_8xye", x >> 8, y >> 4
+    if opcode == 9:
+        if instruction & 0x000f == 0:
+            x = instruction & 0x0f00
+            y = instruction & 0x00f0
+            return "op_9xy0", x >> 8, y >> 4
+    if 0xa <= opcode <= 0xb:
+        return f"op_{opcode:1x}nnn", instruction & 0x0fff
+    if opcode == 0xc:
+        x = instruction & 0x0f00
+        kk = instruction & 0x00ff
+        return "op_cxkk", x >> 8, kk
+    if opcode == 0xd:
+        x = instruction & 0x0f00
+        y = instruction & 0x00f0
+        n = instruction & 0x000f
+        return "op_dxyn", x >> 8, y >> 4, n
+    if opcode == 0xe:
+        function = instruction & 0x00ff
+        x = instruction & 0x0f00
+        if function == 0x9e:
+            return "op_ex9e", x >> 8
+        if function == 0xa1:
+            return "op_exa1", x >> 8
+    if opcode == 0xf:
+        function = instruction & 0x00ff
+        if function in (0x07, 0x0a, 0x15, 0x18, 0x1e, 0x29, 0x33, 0x55, 0x65):
+            x = instruction & 0x0f00
+            return f"op_fx{function:02x}", x >> 8
+    return "",
+
+
