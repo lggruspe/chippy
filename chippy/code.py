3df94fc3bf6ae100204c504a03c739c115bd7847
Levi Gruspe <mail.levig@gmail.com>
2020-08-14 11:05:42 +0800

    Add cleaner dispatch function

--- chippy/code.py
+++ chippy/code.py
@@ -342,3 +342,80 @@ def handle_instruction(cls, instruction, chip8=None, *, check=True):
         raise ChippyError(message)
     handler = getattr(cls, name)
     return handler(chip8, *args)
+
+def dispatch(instruction, impl):
+    """Run instruction on implementation."""
+
+    opcode = instruction >> 12
+    x = (instruction & 0xf00) >> 8
+    y = (instruction & 0xf0) >> 4
+    nnn = instruction & 0xfff
+    kk = instruction & 0xff
+    funct = instruction & 0xf
+
+    if opcode == 0:
+        if kk == 0xe0:
+            return impl.op_00e0()
+        elif kk == 0xee:
+            return impl.op_00ee()
+    elif opcode == 1:
+        return impl.op_1nnn(nnn)
+    elif opcode == 2:
+        return impl.op_2nnn(nnn)
+    elif opcode == 3:
+        return impl.op_3xkk(x, kk)
+    elif opcode == 4:
+        return impl.op_4xkk(x, kk)
+    elif opcode == 5:
+        if funct == 0:
+            return impl.op_5xy0(x, y)
+    elif opcode == 6:
+        return impl.op_6xkk(x, kk)
+    elif opcode == 7:
+        return impl.op_7xkk(x, kk)
+    elif opcode == 8:
+        if 0 <= funct <= 7:
+            return {
+                0: lambda: impl.op_8xy0(x, y),
+                1: lambda: impl.op_8xy1(x, y),
+                2: lambda: impl.op_8xy2(x, y),
+                3: lambda: impl.op_8xy3(x, y),
+                4: lambda: impl.op_8xy4(x, y),
+                5: lambda: impl.op_8xy5(x, y),
+                6: lambda: impl.op_8xy6(x, y),
+                7: lambda: impl.op_8xy7(x, y),
+            }[funct]()
+        elif funct == 0xe:
+            return impl.op_8xye(x, y)
+    elif opcode == 9:
+        if funct == 0:
+            return impl.op_9xy0(x, y)
+    elif opcode == 0xa:
+        return impl.op_annn(nnn)
+    elif opcode == 0xb:
+        return impl.op_bnnn(nnn)
+    elif opcode == 0xc:
+        return impl.op_cxkk(x, kk)
+    elif opcode == 0xd:
+        n = funct
+        return impl.op_dxyn(x, y, n)
+    elif opcode == 0xe:
+        if kk == 0x9e:
+            return impl.op_ex9e(x)
+        elif kk == 0xa1:
+            return impl.op_exa1(x)
+    elif opcode == 0xf:
+        case = {
+            0x07: lambda: impl.op_fx07(x),
+            0x0a: lambda: impl.op_fx0a(x),
+            0x15: lambda: impl.op_fx15(x),
+            0x18: lambda: impl.op_fx18(x),
+            0x1e: lambda: impl.op_fx1e(x),
+            0x29: lambda: impl.op_fx29(x),
+            0x33: lambda: impl.op_fx33(x),
+            0x55: lambda: impl.op_fx55(x),
+            0x65: lambda: impl.op_fx65(x),
+        }.get(kk)
+        if case:
+            return case()
+    return ChippyError(f"Invalid instruction: {instruction:#06x}")
